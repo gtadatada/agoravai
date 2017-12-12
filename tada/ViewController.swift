d2956ef9142231c6b272a5048976c9f8e3d8ffa2
George Tada <>
2017-12-12 09:34:36 -0200

    test auto

---
+++ tada/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  tada
+//
+//  Created by George Tada on 04/12/17.
+//  Copyright © 2017 George Tada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var peso: UITextField!
+    @IBOutlet weak var altura: UITextField!
+    @IBOutlet weak var resultado: UILabel!
+    @IBOutlet weak var iMagemResultado: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBAction func bottonCalcular(_ sender: Any) {
+    }
+    
+
+}
+
